7dd9fe5e42aedb977a71fa693faa56bc88afc9c1
Princess Wilson <aurorapwilson1@gmail.com>
2019-02-06 10:16:55 -0500

    artists object

--- keys.js
+++ keys.js
@@ -1,38 +1,9 @@
 
 //spotify request
-let Spotify = require ('node-spotify-api');
 
 
-let spotify = new Spotify({
-    id: '8442d6c3ea83457aaecd79e844fc0769',
-    secret: 'aef599a9eadf4daca3e2ae1c08a4429a'
-  });
-  //spotify api call
-  var songName = process.argv[2];
-  spotify.search( {type: "track", query: songName },function(err, data) {
-   
-    if(!songName){
-      songName = "What's my age again";
-    }
-  
-  
-      if (err) {
-        return console.log('Error occurred: ' + err)
-      }
-  else {
-    for(var i = 0; i < data.tracks.items[0].artists.length; i++){
-      if (i===0){
-        console.log("Artists(s):      " + data.tracks.items.artists.
-      }
-      else{
-        console.log("                 " + data.tracks.items.artists.
-      }
-        console.log("Song:            " + data.tracks.items[0].name);
-        console.log("Preview Link:            " + data.tracks.items[0].preview_url);
-        console.log("AlbumS:            " + data.tracks.items[0].album.name);
-  
-    var json = data.tracks.items[0].artists.name;
-    console.log(json);
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+    
   }
-      }
-    });
--- liri.js
+++ liri.js
@@ -4,6 +4,10 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 
+let Spotify = require ('node-spotify-api');
+
+
+let spotify = new Spotify(keys.spotify);
 
 //variables to store CL input
 
@@ -38,23 +42,57 @@ const searchTerm = process.argv[3];//movie title, concert, songs title.
 
     //  -------------------------------------------------------------------------------------//
     //functions 
-    // function spotifySong(){
-    //     if (liriArgument === "spotify-this-song"){
+    function spotifySong(){
+
            
-    //     };
+    //spotify api call
+    
+    spotify.search( {type: "track", query: searchTerm },function(err, data) {
+     
+      if(!searchTerm){
+        searchTerm = "What's my age again";
+      }
+    
+    
+        if (err) {
+          return console.log('Error occurred: ' + err)
+        }
+    else {
+    //   for(var i = 0; i < data.tracks.items[0].artists.length; i++){
+    //     if (i===0){
+    //       console.log("Artists(s):      " + data.tracks.items.artists[0].name);
+
+    //     }
+    //     else{
+    //       console.log("                 " + data.tracks.items.artists[0].name);
+    //     }
+    //       console.log("Song:            " + data.tracks.items[0].name);
+    //       console.log("Preview Link:            " + data.tracks.items[0].preview_url);
+    //       console.log("AlbumS:            " + data.tracks.items[0].album.name);
+    
+   
+    // }
+
+console.log(JSON.stringify(data.tracks.items[0].album.artists.name));
+
+    
+      };
+    });
+  }
+        
     //axios api call to ombd and bandsintown
 //look up axios require
 var axios = require ('axios');
 //axios api call
 
 
-axios.get("http://www.omdbapi.com/?t="+searchTerm+"&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    // Then we print out the imdbRating
+// axios.get("http://www.omdbapi.com/?t="+searchTerm+"&y=&plot=short&apikey=trilogy").then(
+//   function(response) {
+//     // Then we print out the imdbRating
     
-    console.log(response);
-    console.log("The movie's rating is: " + response.data.imdbRating);
-  });
+//     // console.log(response);
+//     // console.log("The movie's rating is: " + response.data.imdbRating);
+//   });
 
 
     
@@ -65,5 +103,6 @@ axios.get("http://www.omdbapi.com/?t="+searchTerm+"&y=&plot=short&apikey=trilogy
 
 
     
+
 
 
